18604dc4f32c46025773c43000f26b2d477f4cce
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2020-11-13 19:42:08 -0300

    feat: add basic linkedlist classes

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -1,1 +1,14 @@
-class LinkedList {}
+const Node = require("./Node");
+
+class LinkedList {
+  constructor() {
+    this.first;
+    this.last;
+  }
+  addFirst() {}
+  addLast() {}
+  deleteFirst() {}
+  deleteLast() {}
+  contains() {}
+  indexOf() {}
+}
